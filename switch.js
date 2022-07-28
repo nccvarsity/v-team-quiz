b78e382fc51282f9c356fb93cebef81dba0ec387
keanotv <lingcherkeane@gmail.com>
2022-07-29 01:07:59 +0800

    Change questions, make switch fun

--- switch.js
+++ switch.js
@@ -1,11 +1,13 @@
 function flickSwitch() {
   // Select the stylesheet <link>
   const theme = document.getElementById("theme-link");
-  if (theme.getAttribute("href") == "app.css") {
+  if (dark) {
     // ... then switch it to "dark-theme.css"
     theme.href = "app-dark.css";
-    document.querySelectorAll("ellipse, path").forEach(function(item) {
-      item.setAttribute("stroke", "white");
+    document.querySelectorAll("ellipse, path").forEach(function(item, index) {
+      setTimeout(function() {
+        item.setAttribute("stroke", "white");
+      }, 30 * (index + 1));
     });
     document.querySelectorAll("button").forEach(function(item) {
       item.classList.toggle("border-black");
@@ -18,8 +20,10 @@ function flickSwitch() {
   } else {
     // ... switch it to "light-theme.css"
     theme.href = "app.css";
-    document.querySelectorAll("ellipse, path").forEach(function(item) {
-      item.setAttribute("stroke", "black");
+    document.querySelectorAll("ellipse, path").forEach(function(item, index) {
+      setTimeout(function() {
+        item.setAttribute("stroke", "black");
+      }, 30 * (index + 1));
     });
     document.querySelectorAll("button").forEach(function(item) {
       item.classList.toggle("border-black");
